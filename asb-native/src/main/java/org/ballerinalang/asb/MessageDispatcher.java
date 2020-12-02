8319fb9ce2ca60f2b7d3611b932f7a33f8fd73ea
RolandHewage <roland.hewage@gmail.com>
2020-12-02 17:03:11 +0530

    Add documentation comments

--- asb-native/src/main/java/org/ballerinalang/asb/MessageDispatcher.java
+++ asb-native/src/main/java/org/ballerinalang/asb/MessageDispatcher.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.ballerinalang.asb;
 
 import com.google.gson.JsonParser;
@@ -29,8 +47,10 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.ballerinalang.asb.AsbConstants.*;
 import static org.ballerinalang.asb.connection.ListenerUtils.isClosing;
 
+/**
+ * Handles and dispatched messages with data binding.
+ */
 public class MessageDispatcher {
-    private String consumerTag;
     private BObject service;
     private String queueName;
     private String connectionKey;
@@ -43,7 +63,6 @@ public class MessageDispatcher {
         this.service = service;
         this.queueName = getQueueNameFromConfig(service);
         this.connectionKey = getConnectionStringFromConfig(service);
-        this.consumerTag = service.getType().getName();
         this.runtime = runtime;
         this.receiver = iMessageReceiver;
     }
